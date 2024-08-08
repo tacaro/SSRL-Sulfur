b81e5d75f6b272ef170c6b9cb67c09a76ea0f917
tacaro <51676080+tacaro@users.noreply.github.com>
2024-08-07 18:38:28 -0700

    Update theme_xas.R

--- source/theme_xas.R
+++ source/theme_xas.R
@@ -4,6 +4,9 @@ theme_xas <- function() {
     panel.background = element_rect(color = "black"),
     strip.background = element_rect(color = "#464747", fill = "#464747"),
     strip.text = element_text(color = "white"),
-    axis.ticks.x = element_blank()
+    axis.ticks.x = element_blank(),
+    axis.ticks.y = element_blank(),
+    axis.text.y = element_blank(),
+    axis.title.y = element_blank()
   )
 }
